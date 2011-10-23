95f00bfeacb9c08fd3fe7709c1f3f106865dfe11
Allan Le Nevez <allanlenevez@hotmail.com>
2011-10-23 14:26:43 +1100

    refactoring main program to create more agents

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from agent import Agent
 from ant import Ant
 from taskfactory import TaskFactory
 
+def create_agents(agents_list, count):
+    for index in range(count):
+        agents_list.append(Agent('agent %d' % (index), 2))
+
+def print_agents(agents_list):
+    for index in range(len(agents_list)):
+        print agents_list[index]
+
 if __name__ == '__main__':
-    agents = [Agent('agent1', 2), Agent('agent2', 2)]
-    for agent in agents:
-        print agent
+    agents = []
+    create_agents(agents, 10)
+    print_agents(agents)
+    print agents
 
     ants = [Ant(agents[0], TaskFactory.get_task('a'), None)]
     ants.append(Ant(agents[1], TaskFactory.get_task('b'), None))
