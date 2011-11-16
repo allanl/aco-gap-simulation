d78b0bddab7c656c876fd46e93e4cc6a0c08f2f9
allanl <allanlenevez@hotmail.com>
2011-11-16 12:13:57 +1100

    returning self.name for string representation to make output more readable

--- task.py
+++ task.py
@@ -3,6 +3,12 @@ class Task(object):
     def process(self, payload):
         print "%s processing %s" % (self.name, payload)
 
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 class TaskA(Task):
     def __init__(self):
         self.name = 'TaskA'
