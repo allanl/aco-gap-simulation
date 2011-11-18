2d600cab2636c986aa9dde2f8c78c2bb891c9dfe
allanl <allanlenevez@hotmail.com>
2011-11-18 21:23:57 +1100

    adding M to edges that are sitting at max pheromone level

--- graph.py
+++ graph.py
@@ -7,7 +7,12 @@ def graph_pheromones(node_list, task, filename):
         if node.has_task(task):
             graph_nodes[node].set_label("%s: %s" % (node.get_name(), task.get_name()))
         for conn in node.get_connections():
-            graph.add_edge(pydot.Edge(graph_nodes[node], graph_nodes[conn.get_node()], label=conn.get_pheromone(task)))
+            edge = pydot.Edge(graph_nodes[node], graph_nodes[conn.get_node()])
+            label_text = ''
+            if conn.get_pheromone(task) == conn.get_max_pheromone(task):
+                label_text = 'M'
+            edge.set_label("%d%s" % (conn.get_pheromone(task), label_text))
+            graph.add_edge(edge)
         graph.add_node(graph_nodes[node])
     graph.write_png('%s.png' % filename)
     
